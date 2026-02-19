4fb40e348c85a74d618f16c31dd80ddafaf91a9a
Bayron110 <bayron124z@hotmail.com>
2026-02-19 09:24:49 -0500

    Version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"Home", component:Home},
+    {path: "", redirectTo:"Home", pathMatch:"full"}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Fotter } from "./components/fotter/fotter";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Fotter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
